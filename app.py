75fe6be613cbe76ea6419dc7b15619db7800668c
diagonalprinter <stonkzbuyer@gmail.com>
2025-12-24 09:34:26 -0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import requests
 import time
 from py_clob_client.client import ClobClient
 from py_clob_client.clob_types import OrderArgs, OrderType
-from py_clob_client.order_builder.constants import BUY, GTC
+from py_clob_client.constants import BUY, GTC  # Both here in the fork
 
 # Constants
 GAMMA_API = "https://gamma-api.polymarket.com/markets"
